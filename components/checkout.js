54b4340eb81de9d83a1200215ab1c4e1ba5c7d9a
Ben Polinsky <benpolinsky@Bens-iMac.local>
2017-01-22 16:47:55 -0500

    toward a basic checkout

--- components/checkout.js
+++ components/checkout.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {View, Text, TextInput, PickerIOS} from 'react-native';
-import USStates from './USStates.js';
+import {View, ScrollView, Text, TextInput, PickerIOS, Button, Modal} from 'react-native';
+import USStates from '../support/USStates.js';
 
 const PickerItemIOS = PickerIOS.Item;
 
@@ -12,13 +12,28 @@ export default class Checkout extends Component {
 			name: "",
 			street: "",
 			city: "",
-			usState: ""
+			usState: "",
+			country: "",
+			cc_number: "",
+			cc_cvc: "",
+			cc_exp: "",
+			modalOpen: false
 		}
+		
+		this.closeModal.bind(this);
+	}
+	
+	submitPurchase(){
+		this.setState({modalOpen: true})
+	}
+	
+	closeModal(){
+		this.setState({modalOpen: false})
 	}
 	
 	render(){
 		return(
-			<View style={{flex: 1, paddingTop: 100, alignItems: 'stretch', justifyContent: 'flex-start'}}>
+			<ScrollView contentContainerStyle={{paddingTop: 100, alignItems: 'stretch', justifyContent: 'flex-start'}}>
 				<MnTextField label="Name:" onChangeText={(text) => this.setState({name: text})} value={this.state.name} />
 				<MnTextField label="Street:" onChangeText={(text) => this.setState({street: text})} value={this.state.street} />
 				<MnTextField label="City:" onChangeText={(text) => this.setState({city: text})} value={this.state.city} />
@@ -33,8 +48,29 @@ export default class Checkout extends Component {
 							label={usState}
 							/>
 					})}
-					</PickerIOS>
-			</View>
+				</PickerIOS>
+				<MnTextField label="Country:" onChangeText={(text) => this.setState({country: text})} value={this.state.country} />
+				<MnTextField label="Credit Card Number:" onChangeText={(text) => this.setState({cc_number: text})} value={this.state.cc_number} />
+				<MnTextField label="Credit Card Security Code:" onChangeText={(text) => this.setState({cc_cvc: text})} value={this.state.cc_cvc} />
+				<MnTextField label="Credit Card Expiry:" onChangeText={(text) => this.setState({cc_exp: text})} value={this.state.cc_exp} />
+				<Button title="Purchase" color="#46A8B9" onPress={this.submitPurchase.bind(this)}/>
+					<Modal 
+						animiationType='slide' 
+						transparent={false} 
+						visible={this.state.modalOpen} 
+						>
+						<Text>Payment Details</Text>
+						<Text>Name: </Text>
+						<Text>Street: </Text>
+						<Text>City: </Text>
+						<Text>State: </Text>
+						<Text>Country: </Text>
+						<Text>Credit Card Number: </Text>
+						<Text>Credit Card CVC: </Text>
+						<Text>Credit Card EXP: </Text>
+						<Button title="close" onPress={this.closeModal.bind(this)} />
+					</Modal>
+			</ScrollView>
 		)
 	}
 }
